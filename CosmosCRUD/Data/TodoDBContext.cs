4568859cb0f06878b5458bfdb4c9eac0bfe4d776
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-06-10 19:11:02 +0800

    Add project files.

---
+++ CosmosCRUD/Data/TodoDBContext.cs
@@ -0,0 +1,27 @@
+﻿using CosmosCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmosCRUD.Data
+{
+    public class TodoDBContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public TodoDBContext()
+        {
+
+        }
+        public TodoDBContext(DbContextOptions<TodoDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // configuring Todos
+            modelBuilder.Entity<Todo>()
+                .ToContainer("Todo")
+                .HasPartitionKey(e => e.Id);
+        }
+    }
+}
---
+++ CosmosCRUD/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmosCRUD.Models
+{
+    public class Todo
+    {
+        public required string Id { get; set; }
+        public required string Title { get; set; } = string.Empty;
+        public required string Description { get; set; } = string.Empty;
+    }
+}
